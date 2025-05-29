7a4203cc1c6edce4823b10caf66c15ebbc4d3125
Rathod Ravi <ravirathod100100@gmail.com>
2025-05-29 20:04:13 +0530

    Add files via upload

---
+++ condition statement/p5.c
@@ -0,0 +1,19 @@
+//chech number is even or odd
+//Even
+//odd
+#include<stdio.h>
+int main()
+{
+	int num;
+	printf("Enter the number =");
+	scanf("%d",&num);
+	if(num(5) % 2 == 0)
+	{
+		printf("\n Even %d",num);		
+	}
+	else
+	{
+		printf("\n Odd %d",num);	
+	}
+	return 0;
+}
