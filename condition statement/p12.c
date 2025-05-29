353dfadd008d7f12634e62b7c18349ae964e94df
Rathod Ravi <ravirathod100100@gmail.com>
2025-05-29 20:12:33 +0530

    Add files via upload

---
+++ condition statement/p12.c
@@ -0,0 +1,49 @@
+//print of day(1-7)
+#include<stdio.h>
+int main()
+{
+	int day;
+	printf("\n Enter The Day [1-7] :");
+	scanf("%d",&day);
+	if(day == 1)
+	{
+		printf("\n Mon");
+	}
+	else if(day == 2)
+	{
+		printf("\n Tue");
+	}
+	else if(day == 3)
+	{
+		printf("\n Wen");
+	}
+	else if(day == 4)
+	{
+		printf("\n Thu");
+	}
+	else if(day == 5)
+	{
+		printf("\n Fri");
+	}
+	else if(day == 6)
+	{
+		printf("\n Sat");
+	}
+	else if(day == 7)
+	{
+		printf("\n Sun");
+	}
+	else if(day == 2)
+	{
+		printf("\n Tue");
+	}
+	else if(day == 3)
+	{
+		printf("\n Wen");
+	}
+	else if(day == 4)
+	{
+		printf("\n Invalid number");
+	}
+	return  0;
+}
