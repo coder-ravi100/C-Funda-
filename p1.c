2345bd75d9a3ba7fa700a3ccae5f9989f6946739
Rathod Ravi <ravirathod100100@gmail.com>
2025-05-29 19:51:16 +0530

    Add files via upload

---
+++ p1.c
@@ -0,0 +1,14 @@
+//Whether a number is positive
+#include<stdio.h>
+int main()
+{
+	int num;
+	printf("\n Enter The Number :");
+	scanf("%d",&num);
+	if(num > 0)
+	{
+		printf("\n %d Number is Positive",num);
+	}
+	
+	return 0;
+}
