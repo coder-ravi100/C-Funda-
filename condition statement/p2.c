c30804052f1cb32f89eb81456d028b59ccfdf181
Rathod Ravi <ravirathod100100@gmail.com>
2025-05-29 20:01:42 +0530

    Add files via upload

---
+++ condition statement/p2.c
@@ -0,0 +1,13 @@
+//Check Whether in a even number
+#include<stdio.h>
+int main()
+{
+	int num;
+	printf("\n Enter the number :");
+	scanf("%d",&num);
+	if(num % 2 == 0)
+	{
+		printf("\n %d This is Even number",num);
+	}
+	return 0;
+}
